92b5c9455d5baa7a282b8a148021228aac106ec7
yurikb8 <yurikb8@gmail.com>
2021-12-14 15:38:42 +0900

    csv作成成功

--- HappyJam_tf/getLearningData_mp.py
+++ HappyJam_tf/getLearningData_mp.py
@@ -1,11 +1,42 @@
 import cv2
 import mediapipe as mp
+import csv
+from collections import deque
+import time
+import argparse
+import itertools
+
+#コマンドライン引数
+parser = argparse.ArgumentParser()
+parser.add_argument("--gesture_id", type=int, default=0) #ラベル種別 0=静止, 1=演奏
+parser.add_argument("--time", type=int, default=10) #録画時間(sec)
+args = parser.parse_args()
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+#csvのパス
+csv_path = './point_history.csv'
+
+start_time = time.time()
+
+tmp_landmarks_list=[]
+
+# 座標履歴を保持するための変数
+history_length = 16
+point_history = deque(maxlen=history_length)
+
+# CSVファイルに座標履歴を保存する関数
+def logging_csv(gesture_id, csv_path, point_history_list):
+    with open(csv_path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([gesture_id, *point_history_list])
+        print("aaaa")
+    return
+
 # For webcam input:
-filepath = "HappyJam_tf/movie/test2.mp4"
+filepath = "./movie/test2.mp4"
 cap = cv2.VideoCapture(filepath) #引数0ならカメラ,filepathで動画
 with mp_pose.Pose(
     model_complexity=1,
@@ -32,8 +63,25 @@ with mp_pose.Pose(
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+    tmp_landmarks_list.append([results.pose_landmarks.landmark[0].x,results.pose_landmarks.landmark[0].y])
+
+    for idx in range(11,25):
+        tmp_landmarks_list.append([results.pose_landmarks.landmark[idx].x,results.pose_landmarks.landmark[idx].y])
+
+    # 人差指の指先座標を履歴に追加
+    point_history.append(tmp_landmarks_list)
+
+    if len(point_history) == history_length:
+        point_history_list = list(itertools.chain.from_iterable(point_history))
+        logging_csv(args.gesture_id, csv_path,point_history_list)
+
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(1) & 0xFF == 27:
-      break
+        break
+
+    if (time.time() - start_time) > args.time:
+        break
+
 cap.release()
