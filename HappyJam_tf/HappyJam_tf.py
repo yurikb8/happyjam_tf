a1466eda206b94b68606fbac11a84377edbcd5b0
yurikb8 <yurikb8@gmail.com>
2022-01-06 11:00:43 +0900

    コメント追加

--- HappyJam_tf/HappyJam_tf.py
+++ HappyJam_tf/HappyJam_tf.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import math
 
 #npzのロード
-path ='npm.npz'
+path ='npm.npz' #npzが保存されてるパス
 with np.load(path) as data:
     landmarks = data['landmarks']
     label = data['label']
@@ -21,12 +21,14 @@ test_labels=label[data_size:][:][:][:]
 train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
 
-BATCH_SIZE = 10
+#バッチサイズ
+BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 100
 
 train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
+#学習モデルの生成
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(16,13,2)),
     tf.keras.layers.Dense(128, activation='relu'),
@@ -38,17 +40,19 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(),
               metrics=['sparse_categorical_accuracy'])
 
 model.trainable = True
+
+#学習開始
 model.fit(train_dataset, epochs=10)
 
+#学習精度確認
 model.evaluate(test_dataset)
 
-#学習確認用
+#推論確認用
 predict_result = model.predict(np.array([test_examples[0]]))
 print(np.squeeze(predict_result))
 print(np.argmax(np.squeeze(predict_result)))
 
 # モデル全体を SavedModel として保存
-
 model.save('saved_model/my_model')
 
 #tflite変換
